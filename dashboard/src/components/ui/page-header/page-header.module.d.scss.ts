15759794b1661d66c7f394f31b4b2c71b88ba24f
Skander Ben Abdelmalak <skander@bentech.app>
2026-03-25 17:00:00 +0100

    feat(dashboard): add index, page header.module.d.scss, page header.module, ... (+1)

---
+++ dashboard/src/components/ui/page-header/page-header.module.d.scss.ts
@@ -0,0 +1,7 @@
+declare const classNames: {
+  readonly header: "header";
+  readonly headerWithAction: "headerWithAction";
+  readonly actions: "actions";
+  readonly headerContent: "headerContent";
+};
+export = classNames;
