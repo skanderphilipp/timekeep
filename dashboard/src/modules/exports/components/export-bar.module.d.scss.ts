6c95f740cd902fd66691a7bf5c000b8a15f40117
Skander Ben Abdelmalak <skander@bentech.app>
2026-05-12 05:00:00 +0100

    feat(dashboard): add export bar.module.d.scss, export bar.module, export bar, ... (+1)

---
+++ dashboard/src/modules/exports/components/export-bar.module.d.scss.ts
@@ -0,0 +1,6 @@
+declare const classNames: {
+  readonly bar: "bar";
+  readonly label: "label";
+  readonly spacer: "spacer";
+};
+export = classNames;
---
+++ dashboard/src/modules/exports/index.ts
@@ -0,0 +1,4 @@
+export { ExportBar } from "./components/export-bar";
+export type { ExportFormat } from "./components/export-bar";
+export { generatePunchReport } from "./lib/generate-punch-report";
+export type { PunchReportData } from "./lib/generate-punch-report";
